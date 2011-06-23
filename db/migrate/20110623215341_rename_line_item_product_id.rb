4f5f8a5dc67a6aee873a204c185f10edd5b45f1f
Katie Zhu <kzhu91@gmail.com>
2011-06-23 15:21:49 -0700

    Deleted line items and cart references in job post model

--- db/migrate/20110623215341_rename_line_item_product_id.rb
+++
@@ -1,9 +0,0 @@
-class RenameLineItemProductId < ActiveRecord::Migration
-  def self.up
-    rename_column(:line_items, :product_id, :job_post_id)
-  end
-
-  def self.down
-    rename_column(:line_items, :job_post_id, :product_id)
-  end
-end
