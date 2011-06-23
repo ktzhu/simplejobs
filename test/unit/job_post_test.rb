0ca45b2a506fe612de0f17add3a79169456acc69
Katie Zhu <kzhu91@gmail.com>
2011-06-23 16:30:41 -0700

    Added job post unit test

--- test/unit/job_post_test.rb
+++ test/unit/job_post_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 
 class JobPostTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "job post attributes must not be empty" do
+    job_post = JobPost.new
+    assert job_post.invalid?
+    assert job_post.errors[:title].any?
+    assert job_post.errors[:company].any?
+    assert job_post.errors[:description].any?
+    assert job_post.errors[:location].any?
+    assert job_post.errors[:experience].any?
+    assert job_post.errors[:skills].any?
+    assert job_post.errors[:industry].any?
+    assert job_post.errors[:pay].any?
   end
 end
