60f8d2dac5794e6780b99fce7f98c4ebe9bba33d
Katie Zhu <kzhu91@gmail.com>
2011-06-24 16:12:17 -0700

    Separated specs into controllers and models subdirs

--- spec/models/job_post_spec.rb
+++ spec/models/job_post_spec.rb
@@ -1,13 +1,45 @@
-class RSpecGreeter
-  def greet
-    "Hello RSpec!"
-  end
-end
+require 'spec_helper'
 
-describe "RSpec Greeter" do
-  it "should say 'Hello RSpec!' when it receives the greet() message" do
-    greeter = RSpecGreeter.new
-    greeting = greeter.greet
-    greeting.should == 'Hello RSpec!'
+describe JobPost do
+  before(:each) do
+    @valid_attributes = {
+      :title => "Software Developer",
+      :company => "Google",
+      :description => "Describe job here",
+      :location => "Mountain View",
+      :experience => "Lots",
+      :skills => "All",
+      :industry => "CS",
+      :pay => 500
+    }
+  end
+  
+  it "creates a valid JobPost given valid attributes" do
+    lambda {
+      JobPost.create(@valid_attributes)
+    }.should change(JobPost, :count).by(1)
+  end
+  
+  it "should save the job title" do
+    j = JobPost.create(@valid_attributes)
+    j.title.should == "Software Developer"
+  end
+  
+  it "must have a company" do
+    lambda {
+      JobPost.create({:company => ''})
+    }.should_not change(JobPost, :count)
+  end
+  
+  it "must have a location" do
+    j = JobPost.new({ :title => "Blah",
+                      :company => "Blahblah",
+                      :description => "Bleh",
+                      :location => '',
+                      :experience => "Blah",
+                      :skills => "None",
+                      :industry => "CS",
+                      :pay => 10})
+    j.should_not be_valid
   end
 end
